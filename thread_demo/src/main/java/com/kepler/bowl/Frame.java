ae57e47026c162f2e97f8a3aa21a280193e74615
shengkai.wang_kepler <alten.wang@daimler.com>
2019-01-15 11:29:55 +0800

    [kepler] update aop dynamic proxy

---
+++ thread_demo/src/main/java/com/kepler/bowl/Frame.java
@@ -0,0 +1,8 @@
+package com.kepler.bowl;
+
+public class Frame {
+
+    public int getScore() {
+        return 0;
+    }
+}
--- thread_demo/src/main/java/com/kepler/bowl/TestFrame.java
+++ thread_demo/src/main/java/com/kepler/bowl/TestFrame.java
@@ -1,7 +1,17 @@
 package com.kepler.bowl;
 
 import junit.framework.*;
+import org.junit.Test;
 
 public class TestFrame extends TestCase {
+    public TestFrame(String name){
+        super((name));
+    }
+
+    @Test
+    public void testScoreNoThrows() {
+        Frame f = new Frame();
+        assertEquals(0, f.getScore());
+    }
 
 }
