3cf48d5f5b9da018bc61086b381baa417ca8c92f
shengkai.wang_kepler <alten.wang@daimler.com>
2019-01-20 23:23:33 +0800

    [kepler] update

---
+++ thread_demo/src/main/java/com/kepler/thread/Demo_StopThreadRun.java
@@ -0,0 +1,15 @@
+package com.kepler.thread;
+
+import org.junit.Test;
+
+public class Demo_StopThreadRun {
+    @Test
+    public void run() throws InterruptedException {
+        Demo_StopThread t = new Demo_StopThread();
+        t.start();
+        Thread.sleep(1000);
+        t.interrupt();
+        System.out.println("is interrupted: " + t.isInterrupted());
+        System.out.println("stop thread ~~~~~");
+    }
+}
