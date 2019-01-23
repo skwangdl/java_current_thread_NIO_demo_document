11ae79f04f5a669c397161a765679535ff52ae08
shengkai.wang_kepler <alten.wang@daimler.com>
2019-01-23 22:18:11 +0800

    [kepler] update

---
+++ thread_demo/src/main/java/com/kepler/thread/Demo_Threadyield.java
@@ -0,0 +1,19 @@
+package com.kepler.thread;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class Demo_Threadyield extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+        long startTime = System.currentTimeMillis();
+        int count = 0;
+        for (int i = 0; i < 10000000; i ++){
+            count = count + 1;
+            Thread.yield();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+}
---
+++ thread_demo/src/main/java/com/kepler/thread/Demo_ThreadyieldRun.java
@@ -0,0 +1,12 @@
+package com.kepler.thread;
+
+import org.junit.Test;
+
+public class Demo_ThreadyieldRun {
+
+    @Test
+    public void run(){
+        Demo_Threadyield d = new Demo_Threadyield();
+        d.start();
+    }
+}
