aaed3ccecbd63ac7d8687c07984aa2dcff4dbb0c
wangck4 <wangck4@lenovo.com>
2019-05-21 16:10:47 +0800

    SK fix

--- demo/src/main/java/com/kepler/thread/Demo_7_synchronized.java
+++ demo/src/main/java/com/kepler/thread/Demo_7_synchronized.java
@@ -1,5 +1,46 @@
 package com.kepler.thread;
 
 public class Demo_7_synchronized extends Thread {
-    
+    private Object lock = new Object();
+
+    synchronized public void method_1() throws InterruptedException {
+        Thread.sleep(1000);
+        for(int i = 0; i < 1000; i ++){
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+
+    public void method_2() throws InterruptedException {
+        Thread.sleep(1000);
+        synchronized (lock){
+            for(int i = 0; i < 1000; i ++){
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+    }
+
+    public void method_3() throws InterruptedException {
+        Thread.sleep(1000);
+        synchronized (this){
+            for(int i = 0; i < 1000; i ++){
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+    }
+
+    public void method_4() throws InterruptedException {
+        Thread.sleep(1000);
+        synchronized (Demo_7_synchronized.class){
+            for(int i = 0; i < 1000; i ++){
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+    }
+
+    synchronized public static void method_5() throws InterruptedException {
+        Thread.sleep(1000);
+        for(int i = 0; i < 1000; i ++){
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
 }
