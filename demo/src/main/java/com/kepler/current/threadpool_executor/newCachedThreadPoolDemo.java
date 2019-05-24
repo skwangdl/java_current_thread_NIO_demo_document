48f0d9b9bcd0035441a4304ab731cd7559bb1cab
wangck4 <wangck4@lenovo.com>
2019-05-24 16:25:08 +0800

    SK fix

---
+++ demo/src/main/java/com/kepler/current/threadpool_executor/newCachedThreadPoolDemo.java
@@ -0,0 +1,34 @@
+package com.kepler.current.threadpool_executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class newCachedThreadPoolDemo {
+    public static void main(String[] args){
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("first begin :" + System.currentTimeMillis());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("first end :" + System.currentTimeMillis());
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("second begin :" + System.currentTimeMillis());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("second end :" + System.currentTimeMillis());
+            }
+        });
+    }
+}
---
+++ demo/src/main/java/com/kepler/current/threadpool_executor/newCachedThreadPoolReUsed.java
@@ -0,0 +1,29 @@
+package com.kepler.current.threadpool_executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class newCachedThreadPoolReUsed {
+    public static void main(String[] args){
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for(int i = 0; i < 10; i ++){
+            executorService.execute(new MyRunnable(("" + i)));
+        }
+    }
+
+    static class MyRunnable implements Runnable{
+        private String name;
+
+        public MyRunnable(String name){
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " username="
+                    + name + " begin: " + System.currentTimeMillis());
+            System.out.println(Thread.currentThread().getName() + " username="
+                    + name + "   end: " + System.currentTimeMillis());
+        }
+    }
+}
