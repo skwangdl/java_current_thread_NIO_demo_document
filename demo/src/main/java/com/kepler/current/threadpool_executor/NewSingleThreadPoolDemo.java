4b8721c8e8e22879cc41940c88a1c3cf521d9c7e
wangck4 <wangck4@lenovo.com>
2019-05-24 17:13:31 +0800

    SK fix

--- demo/src/main/java/com/kepler/current/threadpool_executor/NewSingleThreadPoolDemo.java
+++ demo/src/main/java/com/kepler/current/threadpool_executor/NewSingleThreadPoolDemo.java
@@ -3,10 +3,11 @@ package com.kepler.current.threadpool_executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class NewFixedThreadPoolDemo {
+public class NewSingleThreadPoolDemo {
+
     public static void main(String[] args){
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        for(int i = 0; i < 10; i ++){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for(int i = 0; i < 3; i ++){
             executorService.execute(new MyRunnable(("" + i)));
         }
     }
