078ad31fbab2700b88f8a5f6dafe5a6f6dcf0f47
wangck4 <wangck4@lenovo.com>
2019-05-24 21:07:05 +0800

    SK fix

--- demo/src/main/java/com/kepler/current/callable_future/MyCallableDemo.java
+++ demo/src/main/java/com/kepler/current/callable_future/MyCallableDemo.java
@@ -2,9 +2,9 @@ package com.kepler.current.callable_future;
 
 import java.util.concurrent.*;
 
-public class MyCallable implements Callable<String> {
+public class MyCallableDemo implements Callable<String> {
     private int age;
-    public MyCallable(int age){
+    public MyCallableDemo(int age){
         this.age = age;
     }
 
@@ -13,9 +13,10 @@ public class MyCallable implements Callable<String> {
         Thread.sleep(2000);
         return "return age: " + age;
     }
-
+    // callable 有返回值，可以抛出异常， runnable 无返回值，不可以抛出异常
+    //future.get()具有阻塞性，同步，如果有多个get按顺序执行
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        MyCallable myCallable = new MyCallable(100);
+        MyCallableDemo myCallable = new MyCallableDemo(100);
         ThreadPoolExecutor executor = new ThreadPoolExecutor(2,3,
                 5L, TimeUnit.SECONDS, new LinkedBlockingQueue());
         Future<String> future = executor.submit(myCallable);
