c0f1c7875e8737d35aaeda1ab56c278fd3beab6f
wangck4 <wangck4@lenovo.com>
2019-05-27 21:05:41 +0800

    SK fix

--- demo/src/main/java/com/kepler/selector/DemoSelectorClient_A.java
+++ demo/src/main/java/com/kepler/selector/DemoSelectorClient_A.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 public class DemoSelectorClient_A {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost", 8888);
+        socket.getOutputStream().write("123456".getBytes());
         socket.close();
     }
 }
--- demo/src/main/java/com/kepler/selector/DemoSelectorServer.java
+++ demo/src/main/java/com/kepler/selector/DemoSelectorServer.java
@@ -1,10 +1,14 @@
 package com.kepler.selector;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -13,28 +17,38 @@ public class DemoSelectorServer {
         ServerSocketChannel channel_1 = ServerSocketChannel.open();
         channel_1.bind(new InetSocketAddress("localhost", 8888));
         channel_1.configureBlocking(false);
-        //创建两个通道
-        ServerSocketChannel channel_2 = ServerSocketChannel.open();
-        channel_2.bind(new InetSocketAddress("localhost", 8889));
-        channel_2.configureBlocking(false);
+        //创建通道
         Selector selector = Selector.open();
         //两个通道注册在选择器上
-        channel_1.register(selector, SelectionKey.OP_ACCEPT);
-        channel_2.register(selector, SelectionKey.OP_ACCEPT);
+        SelectionKey selectionKey1 = channel_1.register(selector, SelectionKey.OP_ACCEPT);
         boolean isRun = true;
-            while (isRun == true){
+        while (isRun == true){
             Set<SelectionKey> set = selector.selectedKeys();
             Iterator<SelectionKey> iterator = set.iterator();
             while (iterator.hasNext()){
                 SelectionKey key = iterator.next();
-                ServerSocketChannel channel = (ServerSocketChannel) key.channel();
-                channel.accept();
-                InetSocketAddress ipAddress = (InetSocketAddress) channel.getLocalAddress();
-                System.out.println(ipAddress.getPort() + " be connected");
+                if(key.isAcceptable()){
+                    ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                    ServerSocket serverSocket = channel.socket();
+                    Socket socket = serverSocket.accept();
+                    InetSocketAddress ipAddress = (InetSocketAddress) channel.getLocalAddress();
+                    System.out.println(ipAddress.getPort() + " be connected");
+
+                    InputStream inputStream = socket.getInputStream();
+                    byte[] byteArray = new byte[1024];
+                    int readLength = inputStream.read(byteArray);
+
+                    while (readLength != -1){
+                        String newString = new String(byteArray, 0, readLength);
+                        System.out.println(newString);
+                        readLength = inputStream.read(byteArray);
+                    }
+                    inputStream.close();
+                    socket.close();
+                    iterator.remove();
+                }
             }
         }
         channel_1.close();
-        channel_2.close();
-
     }
 }
